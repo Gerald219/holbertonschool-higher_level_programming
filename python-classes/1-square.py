cb45b1f54b184beeba8f254fea1108771795198c
Gerald Mulero <gdmasm@gmail.com>
2025-05-19 21:39:14 -0400

    Task 1 – added an init method that takes a size value and stores it as a private attribute using self.__size inside the Square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines Square class with private size."""
+
+
+class Square:
+    """Represents a square with a private size."""
+
+    def __init__(self, size):
+        self.__size = size
