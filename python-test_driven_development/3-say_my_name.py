064b39eacd963eb1a5a920fff2d64138c8be2e2f
Gerald Mulero <gdmasm@gmail.com>
2025-05-19 22:44:20 -0400

    Task 2 - this function checks if both name inputs are strings and raises TypeError if not, then prints 'My name is' using .format to display both names properly with spacing

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-This module defines a function that prints a formatted name message.
+This module defines a function that prints a full name.
 """
+
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints: My name is <first name> <last name>
+    Prints: My name is <first_name> <last_name>
+    Raises TypeError if inputs are not strings.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
