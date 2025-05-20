8417d8fd7c6420dfd7b0321d32a414a04dd4a533
Gerald Mulero <gdmasm@gmail.com>
2025-05-19 21:55:45 -0400

    Task 3 – made area method that gets the square’s area by multiplying the size value stored from init

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines Square with area method."""
+
+
+class Square:
+    """Square with validated size and area method."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
