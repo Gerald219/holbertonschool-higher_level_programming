ca8644c61e3773ed63a1c7b0e77ba606d70ddbf0
Gerald Mulero <gdmasm@gmail.com>
2025-05-30 21:16:50 -0400

    recreated 101-lazy_matrix_mul.py after it was flagged as missing. Added correct shebang, docstring, and used numpy.matmul to return the result

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-"""Module that provides lazy matrix multiplication using NumPy.
-"""
-
+"""Function that multiplies 2 matrices using NumPy"""
 import numpy as np
 
-
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies two matrices using NumPy's matmul.
-
-    Args:
-        m_a (list of lists): First matrix.
-        m_b (list of lists): Second matrix.
-
-    Returns:
-        numpy.ndarray: The result of the matrix multiplication.
-    """
+    """Multiplies two matrices using numpy.matmul"""
     return np.matmul(m_a, m_b)
