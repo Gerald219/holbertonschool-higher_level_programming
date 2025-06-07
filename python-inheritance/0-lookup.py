d50b4f8e0a866d4f94baaca79f2622fa4604cf7c
Gerald Mulero <gdmasm@gmail.com>
2025-06-06 22:58:09 -0400

    Added missing blank line to follow PEP8 validation spacing rules in 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that returns available attributes and methods."""
 
+
 def lookup(obj):
     """Returns a list of attributes and methods of the object."""
     return dir(obj)
