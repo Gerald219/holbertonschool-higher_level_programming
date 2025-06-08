7456ccbdc04de3755e28015b64f1a09003058c53
Gerald Mulero <gdmasm@gmail.com>
2025-06-07 20:15:53 -0400

    task 6 = makes use of area() to BaseGeometry that just raises an exception, with area() is not implemented, so other classes have to make their own version

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry module for future geometry classes."""
+
+
+class BaseGeometry:
+    """A base class for geometry operations."""
+
+    def area(self):
+        """Raises an exception to show this method needs to be implemented."""
+        raise Exception("area() is not implemented")
