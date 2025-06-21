48b6c6b3f72ef785a2b5aaf974faaa8b8c6dda39
Gerald Mulero <gdmasm@gmail.com>
2025-06-21 00:08:04 -0400

    Fixed line length in 2-append_write.py.

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Appends a string at the end of a text file and returns the number of characters added."""
+"""This module defines a function that appends text to a file and
+returns the number of characters added.
+"""
 
 
 def append_write(filename="", text=""):
-    """Appends text to a file and returns number of characters written."""
+    """Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added.
+    """
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
