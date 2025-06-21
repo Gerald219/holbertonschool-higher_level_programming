b2dd54710f545c921af63d65dc26818a83053e8c
Gerald Mulero <gdmasm@gmail.com>
2025-06-21 00:04:00 -0400

    Implemented missing module docstring. The error was: 'Module is documented' = FAILED, added a docstring at the top of 2-append_write.py describing what the file does.

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Appends a string at the end of a text file and returns the number of characters added."""
 
 
 def append_write(filename="", text=""):
-    """Appends text to a file and returns the number of characters written"""
+    """Appends text to a file and returns number of characters written."""
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
