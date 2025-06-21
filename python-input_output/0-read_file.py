cab2f5730c3a6de2e0a767b8487448a61ee7a946
Gerald Mulero <gdmasm@gmail.com>
2025-06-20 22:26:51 -0400

    Missing blank line before function in 0-read_file.py to comply with PEP8 (E302). Now passes.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """Module for reading and printing the content of a UTF8 text file to stdout.
 """
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding='utf-8') as f:
