c3c6ae7d312f177ab0621e5b1d5da40d8c977890
Gerald Mulero <gdmasm@gmail.com>
2025-06-21 22:17:28 -0400

    reduced long lines over 79 characters and removed extra blank line.

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Defines a Student class"""
+"""Defines a class Student that can be saved and reloaded from JSON."""
 
 
 class Student:
-    """Represents a student"""
+    """Student class with attributes and JSON conversion tools."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a student with name and age"""
+        """Initialize a new student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns dictionary of student attributes"""
-        if isinstance(attrs, list) and all(type(elem) is str for elem in attrs):
-            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        """Return dictionary version of the instance.
+        If attrs is a list of strings, only return those keys.
+        """
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
+            return {k: getattr(self, k)
+                    for k in attrs if hasattr(self, k)}
         return self.__dict__
 
-
     def reload_from_json(self, json):
-        """Updates attributes from a dictionary."""
-        for key in json:
-            setattr(self, key, json[key])
+        """Update attributes from a dictionary."""
+        for k, v in json.items():
+            setattr(self, k, v)
