24470ad2619c7d898fe250269b5633d91ade798e
Gerald Mulero <gdmasm@gmail.com>
2025-06-21 22:00:00 -0400

    shortened line 14 in 10-student.py,  79-char rule.

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Defines a Student class with selective JSON representation."""
+"""Defines a Student class"""
 
 
 class Student:
-    """Represents a student with basic attributes."""
+    """Represents a student"""
 
     def __init__(self, first_name, last_name, age):
+        """Initializes a student with name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns a dictionary of selected attributes for JSON serialization."""
-        if isinstance(attrs, list):
-            return {key: getattr(self, key)
-                    for key in attrs if hasattr(self, key)}
+        """Returns dictionary of student attributes"""
+        if isinstance(attrs, list) and all(type(elem) is str for elem in attrs):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
         return self.__dict__
