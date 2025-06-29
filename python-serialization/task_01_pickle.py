ed726de1ff42cfb0b784f5f1d2289ee9876d1446
Gerald Mulero <gdmasm@gmail.com>
2025-06-28 20:28:46 -0400

    Task 1 - file task_01_pickle.py, this block defines CustomObject class with __init__ storing name, age, and is_student, working like a profile card blueprint where each object holds these details.

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
