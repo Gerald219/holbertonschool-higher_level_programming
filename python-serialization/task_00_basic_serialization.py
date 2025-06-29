6f30316daf8b30cd9641ae83b3d0d3c8f9025c03
Gerald Mulero <gdmasm@gmail.com>
2025-06-28 20:07:47 -0400

    block 2: load_and_deserialize this block adds reading of a json file back into a dictionary with open() and json.load(), it's comparable to pulling out a saved list to use again.

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -7,3 +7,8 @@ import json
 def serialize_and_save_to_file(data, filename):
     with open(filename, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4)
+
+
+def load_and_deserialize(filename):
+    with open(filename, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
