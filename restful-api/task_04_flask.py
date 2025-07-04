ffaa4b2f8069cfd89a957af976695bac83e944d3
Gerald Mulero <gdmasm@gmail.com>
2025-07-04 15:59:26 -0400

    Task 4:  /add_user POST route so the api can accept JSON data to create new users and store them in memory, delivers message and the saved user details

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -25,5 +25,27 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
+from flask import request
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+
+    return jsonify({
+        "message": "User added",
+        "user": users[username]
+    }), 201
+
 if __name__ == "__main__":
     app.run()
