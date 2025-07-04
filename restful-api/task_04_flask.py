929eff77b9e3d8bc6435c31e27bbdefac87194d2
Gerald Mulero <gdmasm@gmail.com>
2025-07-04 15:45:54 -0400

    Task 4 -- new addition = /status endpoint that responds with plain text 'OK' so we can quickly confirm the api is online and reachable.

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+# In-memory users dictionary (empty at start)
+users = {}
+
+@app.route("/data")
+def get_usernames():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def status():
+    return "OK"
 
 if __name__ == "__main__":
     app.run()
