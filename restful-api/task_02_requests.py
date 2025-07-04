1291556da9e69d1daf1116ad46c8d2de153b3068
Gerald Mulero <gdmasm@gmail.com>
2025-07-03 21:32:41 -0400

    Task 2: file command that connects to an API to collect posts, shows all their titles on the screen, and also puts the data neatly into a CSV file for later use.

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+def fetch_and_print_posts():
+    """Get posts and print their titles"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print("Response Code:", response.status_code)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data:
+            print(post["title"])
+
+def fetch_and_save_posts():
+    """Get posts and save them into a CSV file"""
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        posts_list = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in data]
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts_list)
