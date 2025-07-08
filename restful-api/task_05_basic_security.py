325d9bef0b4e46f7dfd9bcfe5a23373cad98200c
Gerald Mulero <gdmasm@gmail.com>
2025-07-07 22:10:27 -0400

    set up password again login using basic auth, saved users with roles and safe passwords, made a function to check username and password match, added a protected route that only lets real users in, fixed broken code at the top where the tools were mixed together

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import (
+    JWTManager, create_access_token, jwt_required, get_jwt_identity
+)
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -22,3 +24,15 @@ users = {
         "role": "admin"
     }
 }
+
+@auth.verify_password
+def verify_password(username, password):
+    user = users.get(username)
+    if user and check_password_hash(user["password"], password):
+        return username
+
+@app.route("/basic-protected")
+@auth.login_required
+def basic_protected():
+    return "Basic Auth: Access Granted"
+
