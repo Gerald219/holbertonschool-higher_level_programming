741913631fd31ffcadf3487a14ab108ec7734fa9
Gerald Mulero <gdmasm@gmail.com>
2025-07-09 00:14:14 -0400

    corrected syntax by putting star inside count.

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- 8th - counts how many rows in first_table have id equal to 89
 -- this counts how many times 89 appears in the id column
-SELECT COUNT * FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
