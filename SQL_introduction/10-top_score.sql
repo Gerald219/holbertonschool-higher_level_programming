d184adef2fb5ac4a60a8d021460232e4167b2c2b
Gerald Mulero <gdmasm@gmail.com>
2025-07-09 16:22:28 -0400

    output changed by selecting only score and name instead of all columns, used order by score desc to sort from highest to lowest

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
--- task 10 - list all rows from second_table sorted by score from highest to lowest
-SELECT * FROM second_table
+-- task 10 - list score and name from second_table sorted best to worst
+SELECT score, name FROM second_table
 ORDER BY score DESC;
-
