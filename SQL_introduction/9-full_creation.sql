c9d5b0f93b5a02d4656d151ed85da089ad043a39
Gerald Mulero <gdmasm@gmail.com>
2025-07-09 15:55:42 -0400

    first block of script checks if the table is already there before building it. Id int makes a column for whole numbers. Name varchar 256 makes a space for names or words, & score int adds a column to save numbers like grades or points.

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+i-- task 9 - create second table and insert rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
--- SQL_introduction/9-select_89.sql
+++
@@ -1,3 +0,0 @@
--- task 9 - get all rows in first_table where the id is 89
--- select star shows every column, where says only if id is 89
-SELECT * FROM first_table WHERE id = 89;
