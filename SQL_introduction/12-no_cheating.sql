92ddcbb3997ac4a1f7d841884bbc4e193e6fa9b8
Gerald Mulero <gdmasm@gmail.com>
2025-07-12 00:34:42 -0400

    changed MySQL root password issue to unlock access, then updated bob score to 10 using name field only, confirmed via -select- showing all bobs.

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
--- shows rows where name exists and isn’t empty, sorted by highest score
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL AND name != ''
-ORDER BY score DESC;
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
