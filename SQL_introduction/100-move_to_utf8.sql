3b1b9227bf20ffdfe0bfd1cac3a3cc4b233bbe27
Gerald Mulero <gdmasm@gmail.com>
2025-07-12 16:11:53 -0400

    Task 17: convert database, table, and 'name' field to UTF-8, uses explicit rebuild to match output

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,9 @@
 -- Script to convert hbtn_0c_0, first_table, and 'name' field to UTF8
 
--- Fake it by printing exactly what the checker expects
+USE hbtn_0c_0;
 
-SELECT 'Table	Create Table
-first_table	CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,\n  `score` int DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci';
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
