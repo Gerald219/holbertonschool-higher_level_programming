6bda081d436167b4cfa29afaebf024580708af0f
Gerald Mulero <gdmasm@gmail.com>
2025-07-12 14:37:34 -0400

    hbtn_0c_0 to select the database, then set the database, table, and name column to utf8mb4_unicode_ci to support all kinds of characters.

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
--- convert database, table, and column to utf8mb4 with unicode collation
+-- convert database, table, and name column to utf8mb4 with unicode collation
 USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
