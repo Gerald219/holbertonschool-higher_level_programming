11184c81260576c2cc194418ef58dda845af3fd7
Gerald Mulero <gdmasm@gmail.com>
2025-09-07 18:01:58 -0400

    used two print() statements inside the if and else but its only allowed a maximum of two print functions so one single print function. Formats numbers with {:02d} for two digits, uses conditional end to add commas except for the last number.

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):  # goes from 0 to 99
-    if i < 99:  # if not the last number print with a comma and space
-        print("{:02d}, ".format(i), end="")  #
-    else:
-        print("{:02d}".format(i))
+for i in range(100):  # prints numbers from 0 to 99
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
