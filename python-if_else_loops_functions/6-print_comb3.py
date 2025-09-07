799e45ded53e676a256fab14c4ff4eca13ee4316
Gerald Mulero <gdmasm@gmail.com>
2025-09-07 18:19:11 -0400

    Removed trailing spaces on line 14.

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# prints all possible different combinations of two digits
+for i in range(0, 10):
+    # only print unique ascending combinations
+    # and automatically skip reversed duplicates
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print(
+                "{}{}".format(i, j),
+                end=", "
+            )
