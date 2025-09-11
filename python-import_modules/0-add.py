4827538cb85a2d0d9a3eb8a256bacaf0adbfe0e8
Gerald Mulero <gdmasm@gmail.com>
2025-09-11 13:24:07 -0400

    task 0 - math function add () from add_0, stored in a=1 variable and variable b=2, then used format to sum through add() with print to show the numbers. Lines kept under 79 for style.

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
 
-if __name__ == "__main__":
-    a = 1
+from add_0 import add  # Activates the function
+
+if __name__ == "__main__":  # checks if the file is the core one
+    a = 1  # saves values in variables
     b = 2
+
+    # format() result. add() does math. print() prints.
     print("{} + {} = {}".format(a, b, add(a, b)))
