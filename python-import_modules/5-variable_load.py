97135b83ae5028a496b1d21bd96aea8ebf6c9a19
Gerald Mulero <gdmasm@gmail.com>
2025-09-11 21:12:52 -0400

    5-variable_load.py - used from, to go into file variable_load_5 and used python function import, to pull out only the variable named a and show what it has inside.

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from variable_load_5 import a  # import the variable a from file
+
+if __name__ == "__main__":  # run only if this is the main file
+    print(a)  # show the value stored in a
