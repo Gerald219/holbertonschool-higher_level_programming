aa9a28be895a46cedead87ffc9538842d317a40c
Gerald Mulero <gdmasm@gmail.com>
2025-09-13 23:25:00 -0400

    eliminated left trailing space on line 6.

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):  # print a matrix of integers
     for row in matrix:  # go through each row
         for i, num in enumerate(row):  # gives position i and number num
-            end_char = " " if i < len(row) - 1 else ""  
+            end_char = " " if i < len(row) - 1 else ""
             # adds a space unless it’s the last number
             print("{:d}".format(num), end=end_char)  # print number
         print()  # new line after each row
