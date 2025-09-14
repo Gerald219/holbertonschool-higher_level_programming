77a69e986087de3a85f0374eb7786009d859d124
Gerald Mulero <gdmasm@gmail.com>
2025-09-14 19:07:00 -0400

    Function update_dictionary adds or updates a key(label) with its value inside a dictionary.  Checks if the label already exist (=),if the label is already present the value gets replaced with the new one. Otherwise the key(label is created. This returns the updated dictionary with the new or changed pair included.

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    # value is the subject we want to add to the dictionary
+    a_dictionary[key] = value  # does label already exist?
+    return a_dictionary  # return the changed dictionary
