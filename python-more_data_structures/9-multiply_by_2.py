a8bb83af926f5b894940eff2593e730dc9f67d6c
Gerald Mulero <gdmasm@gmail.com>
2025-09-14 20:15:22 -0400

    removed trailing blank line at end of file.

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -6,4 +6,3 @@ def multiply_by_2(a_dictionary):  # double all values in dict
     for k, v in a_dictionary.items():  # loop through key and value
         new_dict[k] = v * 2  # keep the key, values are multiplied by 2
     return new_dict
-
