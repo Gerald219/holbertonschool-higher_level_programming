760e745acae1816c65d4343df5a7276a03176c91
Gerald Mulero <gdmasm@gmail.com>
2025-09-21 15:29:13 -0400

    missing blank line before class definition to resolve pycodestyle error

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,6 +2,7 @@
 
 """Square module that checks size and gets area."""
 
+
 class Square:
     """Square with size check and area."""
 
@@ -10,8 +11,8 @@ class Square:
         if not isinstance(size, int):  # if size is not a number
             raise TypeError("size must be an integer")  # stop: must be int
         if size < 0:  # if size is negative
-            raise ValueError("size must be >= 0")  # stop: must be 0 or more
-        self.__size = size  # keep size hidden inside
+            raise ValueError("size must be >= 0")  # stop must be 0 or more
+        self.__size = size  # keep the size hidden inside
 
     def area(self):  # get area of square
         """Return area (size * size)."""
