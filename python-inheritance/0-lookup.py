e32e31fbc83da844b094cf7eacd512c8264c6f10
Gerald Mulero <gdmasm@gmail.com>
2025-10-03 23:46:24 -0400

    pep8 error, added extra blank line before function and newline at end of file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a function that lists attributes and methods of an object."""
 
+
 def lookup(obj):
     """return the list of available attributes."""
     return dir(obj)
