d8ee78e014b31dc99735f2e68068d7206e76944c
Gerald Mulero <gdmasm@gmail.com>
2025-10-05 17:36:10 -0400

    BaseGeometry class  has now applied area() from raise Exception - which does not do an action or says its not ready, simply raises an exception saying area() is not implemented. confirms method must be defined by child classes.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class with an unimplemented code."""
+
+
+class BaseGeometry:
+    """Base class for geometry shapes."""
+
+    def area(self):
+        """Raise an exception because area is not ready yet."""
+        raise Exception("area() is not implemented")
