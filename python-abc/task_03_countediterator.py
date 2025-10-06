9af59632268b205d3ef6439f8e2d936535933009
Gerald Mulero <gdmasm@gmail.com>
2025-10-06 10:39:58 -0400

    custom CountedIterator class with placeholder ready for iterator logic

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines CountedIterator class that counts how many items were iterated."""
+
+
+class CountedIterator:
+    """Iterator that counts how many items have been used."""
+    pass
