9dd54ac150e7b5a33c5a790d916cecb037794830
Gerald Mulero <gdmasm@gmail.com>
2025-10-06 11:59:04 -0400

    CountedIterator, counts items each time  when next() is called, works in for loops, and get_count() shows total iterated.

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
-"""Defines CountedIterator class that counts how many items were iterated."""
+"""CountedIterator class that counts how many items were iterated."""
 
 
 class CountedIterator:
     """Iterator that counts how many items have been iterated."""
 
     def __init__(self, iterable):
-        """Initialize the iterator and set up the counter."""
-        self.iterator = iter(iterable)  # create an iterator from any iterable
-        self.count = 0  # start counter at zero
+        """Prepare the iterator and start the counter."""
+        self.iterator = iter(iterable)  # turn the list into a iterator
+        self.count = 0  # starting position
+
+    def __iter__(self):
+        """Let Python know this object can be looped through."""
+        return self
+
+    def __next__(self):
+        """Get the next item and increase the counter."""
+        try:
+            item = next(self.iterator)  # get next item from list
+        except StopIteration:
+            raise  # stop ->there are no items left
+        else:
+            self.count += 1  # add +1 to counter
+            return item  # return the current item
+
+    def get_count(self):
+        """Return how many items have been taken so far."""
+        return self.count
