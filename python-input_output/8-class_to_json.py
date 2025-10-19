d4d91f4528451ad56dab369624048cdf61df568d
Gerald Mulero <gdmasm@gmail.com>
2025-10-19 16:23:08 -0400

    added missing blank line before the function definition to meet spacing rules.

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Turn a class instance into a simple dictionary."""
 
+
 def class_to_json(obj):
     """dictionary with all readable attributes of an object."""
     return obj.__dict__
